0d5217283f70907e1d08be25be578b92b133b060
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-06-15 16:39:16 +0530

    [Pradip] add.Checking Largest Numbers Which IS

---
+++ LargestNumber.java
@@ -0,0 +1,27 @@
+package leap;
+import java.util.*;
+
+public class LargestNumbers {
+
+	
+	
+	    public static void main(String[] args) 
+	    {
+	        System.out.print("Enter the length of the array : ");
+	        Scanner sc=new Scanner(System.in);
+	        int len=sc.nextInt();
+	        int[] arr=new int[50];
+	        System.out.print("Enter the elements of the array : ");
+	        for(int i=0;i<len;i++)
+	        {
+	            arr[i]=sc.nextInt();
+	        }
+	        int large = arr[0];
+	        for(int i=0;i<len;i++) { if(arr[i]>large)
+	            {
+	                large=arr[i];
+	            } 
+	        }
+	        System.out.print("The largest element of the array : "+large);
+	    }
+	}
